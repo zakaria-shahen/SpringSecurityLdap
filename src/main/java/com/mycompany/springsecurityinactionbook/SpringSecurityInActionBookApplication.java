cf518b4f9a844b1875ea9160831f7671edc7f305
zakaria-shahen <zakariay.shahen@gmail.com>
2022-11-16 15:59:20 +0200

    initialization

---
+++ src/main/java/com/mycompany/springsecurityinactionbook/SpringSecurityInActionBookApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.springsecurityinactionbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityInActionBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityInActionBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mycompany/springsecurityinactionbook/SpringSecurityInActionBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.springsecurityinactionbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityInActionBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

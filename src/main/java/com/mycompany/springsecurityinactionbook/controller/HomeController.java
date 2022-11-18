82d73e2f2525ffe9940812975878aa3e6ad36a89
zakaria-shahen <zakariay.shahen@gmail.com>
2022-11-18 20:11:59 +0200

    move security context to other thread not mangament by spring uing DelegatingSecurityContextCallable or DelegatingSecurityContextRunnable

--- src/main/java/com/mycompany/springsecurityinactionbook/controller/HomeController.java
+++ src/main/java/com/mycompany/springsecurityinactionbook/controller/HomeController.java
@@ -1,17 +1,34 @@
 package com.mycompany.springsecurityinactionbook.controller;
 
+import org.springframework.security.concurrent.DelegatingSecurityContextCallable;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @RestController
 public class HomeController {
 
     @GetMapping("/home")
-    public Map<String, Object> get() {
-        return Map.of(
-                "page", "home Page"
-        );
+    public Map<String, Object> get() throws ExecutionException, InterruptedException {
+
+        Callable<String>  task = () -> SecurityContextHolder.getContext().getAuthentication().getName();
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        try {
+            return Map.of(
+                    "page", "home Page :" + executorService.submit(
+                            new DelegatingSecurityContextCallable<>(task)
+                    ).get()
+            );
+
+        } finally {
+              executorService.shutdown();
+        }
     }
 }
